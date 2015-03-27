9eb6ab78b2019d3848f5caaf7874904ede1c1d09
ternst55 <ternst@Aerotech.com>
2015-03-27 11:53:58 -0400

    ISSUE-0001: Project showing documentation error.
    This code demonstrates an error in MRefBuilder.exe (from SandCastle)
    when trying to document overloaded methods with array parameters, that
    are inherited from classes using generics.

--- TestProject/HelloWorld.cs
+++ TestProject/HelloWorld.cs
@@ -13,4 +13,45 @@ namespace TestProject
 			Console.WriteLine("Hello World!");
 		}
 	}
+
+	public class InheritedRandomGeneric : RandomGenericClass<String, int, float> { }
+
+	public class RandomGenericClass<TName, TIndex, TType>
+	{
+		internal RandomGenericClass()
+		{
+			Console.WriteLine(" I am constructing. ");
+		}
+
+		/// <summary>
+		/// Documentation for Overload int.
+		/// </summary>
+		/// <param name="overload">The parameter.</param>
+		/// <returns>The return.</returns>
+		public int[] OverloadedMethod(int[] overload)
+		{
+			return overload;
+		}
+
+		/// <summary>
+		/// Documentation for Overload single.
+		/// </summary>
+		/// <param name="overload">The parameter.</param>
+		/// <returns>The return.</returns>
+		public Single[] OverloadedMethod(Single[] overload)
+		{
+			return overload;
+		}
+
+		/// <summary>
+		/// Documentation for Overload double.
+		/// </summary>
+		/// <param name="overload">The parameter.</param>
+		/// <returns>The return</returns>
+		public double[] OverloadedMethod(double[] overload)
+		{
+			return overload;
+		}
+
+	}
 }
